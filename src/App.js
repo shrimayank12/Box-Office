8205756647ee109671eddf287ea21872d6428428
Mayank Shrivastava <shrimayank12@gmail.com>
2022-06-13 17:36:37 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      Hi
-    </div>
-  );
+  return <Switch>
+
+    <Route exact path='/'>
+      This is Home Page.
+    </Route>
+
+    <Route exact path='/starred'>
+      This is Starred Page.
+    </Route>
+
+    <Route>
+      This is 404 Page.
+    </Route>
+
+  </Switch>;
 }
 
 export default App;
