67b4d500ccbb1429c431f1fe4019d5a935a7fc07
Mayank Shrivastava <shrimayank12@gmail.com>
2022-06-13 18:36:16 +0530

    added layout management and created title

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs/>
+    <Switch>
+      <Route exact path='/'>
+        <Home/>
+      </Route>
 
-      <Switch>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route exact path='/starred'>
-          <Starred/>
-        </Route>
-        <Route>
-          <div>
-            Error 404! Page Not Found.
-          </div>
-        </Route>
-      </Switch>
-    </div>
+      <Route exact path='/starred'>
+        <Starred/>
+      </Route>
+      
+      <Route>
+        <div>
+          Error 404! Page Not Found.
+        </div>
+      </Route>
+    </Switch>
   )
 }
 
